182d4c15544689b3a095aaa6c20781160273bfe4
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2016-11-10 18:32:59 +0900

    [ADD] Implement google and firebase sign in

--- app/src/main/java/com/lakeel/altla/vision/builder/presentation/di/component/ActivityComponent.java
+++ app/src/main/java/com/lakeel/altla/vision/builder/presentation/di/component/ActivityComponent.java
@@ -6,6 +6,7 @@ import com.lakeel.altla.vision.builder.presentation.di.module.PresenterModule;
 import com.lakeel.altla.vision.builder.presentation.di.module.RepositoryModule;
 import com.lakeel.altla.vision.builder.presentation.view.activity.MainActivity;
 import com.lakeel.altla.vision.builder.presentation.view.fragment.MainFragment;
+import com.lakeel.altla.vision.builder.presentation.view.fragment.SignInFragment;
 
 import dagger.Subcomponent;
 
@@ -16,4 +17,6 @@ public interface ActivityComponent {
     void inject(MainActivity activity);
 
     void inject(MainFragment fragment);
+
+    void inject(SignInFragment fragment);
 }
