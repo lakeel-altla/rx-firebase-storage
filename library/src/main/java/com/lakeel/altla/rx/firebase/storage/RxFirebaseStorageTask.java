0cf3bbec539f0973a388b0b52d71f105e5afc28e
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2016-11-21 19:40:37 +0900

    [ADD] Implement methods to wrap a StreamDownloadTask

--- library/src/main/java/com/lakeel/altla/rx/firebase/storage/RxFirebaseStorageTask.java
+++ library/src/main/java/com/lakeel/altla/rx/firebase/storage/RxFirebaseStorageTask.java
@@ -3,6 +3,7 @@ package com.lakeel.altla.rx.firebase.storage;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.StreamDownloadTask;
 import com.google.firebase.storage.UploadTask;
 
 import android.support.annotation.NonNull;
@@ -273,6 +274,137 @@ public final class RxFirebaseStorageTask {
                             new com.google.firebase.storage.OnProgressListener<FileDownloadTask.TaskSnapshot>() {
                                 @Override
                                 public void onProgress(FileDownloadTask.TaskSnapshot snapshot) {
+                                    onProgressListener.onProgress(snapshot.getTotalByteCount(),
+                                                                  snapshot.getBytesTransferred());
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    /**
+     * Wraps the specified {@link StreamDownloadTask} in {@link Observable}.
+     *
+     * @param task               The wrapped task.
+     * @param onProgressListener The callback to get the progress status, or null.
+     * @return The {@link Observable} that wraps the specified {@link StreamDownloadTask}.
+     */
+    @NonNull
+    public static Observable<StreamDownloadTask.TaskSnapshot> asObservable(
+            @NonNull final StreamDownloadTask task, @Nullable final OnProgressListener onProgressListener) {
+        if (task == null) throw new IllegalArgumentException("'task' must be not null.");
+
+        return Observable.create(new Observable.OnSubscribe<StreamDownloadTask.TaskSnapshot>() {
+            @Override
+            public void call(final Subscriber<? super StreamDownloadTask.TaskSnapshot> subscriber) {
+
+                task.addOnSuccessListener(new OnSuccessListener<StreamDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(StreamDownloadTask.TaskSnapshot snapshot) {
+                        subscriber.onNext(snapshot);
+                        subscriber.onCompleted();
+                    }
+                });
+                task.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        subscriber.onError(e);
+                    }
+                });
+
+                if (onProgressListener != null) {
+                    task.addOnProgressListener(
+                            new com.google.firebase.storage.OnProgressListener<StreamDownloadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(StreamDownloadTask.TaskSnapshot snapshot) {
+                                    onProgressListener.onProgress(snapshot.getTotalByteCount(),
+                                                                  snapshot.getBytesTransferred());
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    /**
+     * Wraps the specified {@link StreamDownloadTask} in {@link Single}.
+     *
+     * @param task               The wrapped task.
+     * @param onProgressListener The callback to get the progress status, or null.
+     * @return The {@link Single} that wraps the specified {@link StreamDownloadTask}.
+     */
+    @NonNull
+    public static Single<StreamDownloadTask.TaskSnapshot> asSingle(
+            @NonNull final StreamDownloadTask task, @Nullable final OnProgressListener onProgressListener) {
+        if (task == null) throw new IllegalArgumentException("'task' must be not null.");
+
+        return Single.create(new Single.OnSubscribe<StreamDownloadTask.TaskSnapshot>() {
+
+            @Override
+            public void call(final SingleSubscriber<? super StreamDownloadTask.TaskSnapshot> subscriber) {
+
+                task.addOnSuccessListener(new OnSuccessListener<StreamDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(StreamDownloadTask.TaskSnapshot snapshot) {
+                        subscriber.onSuccess(snapshot);
+                    }
+                });
+                task.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        subscriber.onError(e);
+                    }
+                });
+
+                if (onProgressListener != null) {
+                    task.addOnProgressListener(
+                            new com.google.firebase.storage.OnProgressListener<StreamDownloadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(StreamDownloadTask.TaskSnapshot snapshot) {
+                                    onProgressListener.onProgress(snapshot.getTotalByteCount(),
+                                                                  snapshot.getBytesTransferred());
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    /**
+     * Wraps the specified {@link StreamDownloadTask} in {@link Completable}.
+     *
+     * @param task               The wrapped task.
+     * @param onProgressListener The callback to get the progress status, or null.
+     * @return The {@link Completable} that wraps the specified {@link StreamDownloadTask}.
+     */
+    @NonNull
+    public static Completable asCompletable(@NonNull final StreamDownloadTask task,
+                                            @Nullable final OnProgressListener onProgressListener) {
+        if (task == null) throw new IllegalArgumentException("'task' must be not null.");
+
+        return Completable.create(new Completable.OnSubscribe() {
+            @Override
+            public void call(final CompletableSubscriber subscriber) {
+
+                task.addOnSuccessListener(new OnSuccessListener<StreamDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(StreamDownloadTask.TaskSnapshot snapshot) {
+                        subscriber.onCompleted();
+                    }
+                });
+                task.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        subscriber.onError(e);
+                    }
+                });
+
+                if (onProgressListener != null) {
+                    task.addOnProgressListener(
+                            new com.google.firebase.storage.OnProgressListener<StreamDownloadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(StreamDownloadTask.TaskSnapshot snapshot) {
                                     onProgressListener.onProgress(snapshot.getTotalByteCount(),
                                                                   snapshot.getBytesTransferred());
                                 }
