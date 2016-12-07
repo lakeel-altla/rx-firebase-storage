647dd9c7d7e64d64ddcf3fbb8cc98e5e13d8e1b2
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2016-12-07 18:00:26 +0900

    [UPDATE] Implement the logic to sign in to Firebase with Rx

--- app/src/main/java/com/lakeel/altla/vision/builder/presentation/presenter/SignInPresenter.java
+++ app/src/main/java/com/lakeel/altla/vision/builder/presentation/presenter/SignInPresenter.java
@@ -6,40 +6,34 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import com.lakeel.altla.android.log.Log;
 import com.lakeel.altla.android.log.LogFactory;
 import com.lakeel.altla.vision.builder.R;
+import com.lakeel.altla.vision.builder.domain.usecase.SignInToFirebaseUseCase;
 import com.lakeel.altla.vision.builder.presentation.view.SignInView;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import javax.inject.Inject;
+
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.subscriptions.CompositeSubscription;
 
 /**
  * Defines the presenter for {@link SignInView}.
  */
 public final class SignInPresenter implements GoogleApiClient.OnConnectionFailedListener {
 
-    // NOTE:
-    //
-    // This class does not use any use-case classes because it's difficult to use them.
-    // Google API and Firebase API depend on activities or fragments strongly.
-
     private static final Log LOG = LogFactory.getLog(SignInPresenter.class);
 
     private static final int REQUEST_CODE_GOOGLE_SIGN_IN = 0;
-
-    @Inject
-    Resources resources;
 
     @Inject
     GoogleSignInOptions googleSignInOptions;
@@ -47,9 +41,14 @@ public final class SignInPresenter implements GoogleApiClient.OnConnectionFailed
     @Inject
     AppCompatActivity activity;
 
-    private SignInView view;
+    @Inject
+    SignInToFirebaseUseCase signInToFirebaseUseCase;
+
+    private final CompositeSubscription compositeSubscription = new CompositeSubscription();
 
     private final FirebaseAuth.AuthStateListener authStateListener;
+
+    private SignInView view;
 
     private GoogleApiClient googleApiClient;
 
@@ -64,13 +63,14 @@ public final class SignInPresenter implements GoogleApiClient.OnConnectionFailed
             if (user != null) {
                 if (!mIsSignedInDetected) {
                     LOG.i("Signed in to firebase: %s", user.getUid());
+                    view.hideProgressDialog();
                     view.showMainFragment();
                     mIsSignedInDetected = true;
                 } else {
-                    LOG.d("onAuthStateChanged() fired twice.");
+                    LOG.d("onAuthStateChanged() is fired twice.");
                 }
             } else {
-                LOG.i("signed out from firebase.");
+                LOG.d("Signed out.");
             }
         };
     }
@@ -110,37 +110,37 @@ public final class SignInPresenter implements GoogleApiClient.OnConnectionFailed
             return;
         }
 
-        if (resultCode == Activity.RESULT_OK) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                LOG.i("Google Sign-In succeeded.");
-                LOG.d("Authenticating with Firebase...");
-
-                view.showProgressDialog();
-
-                GoogleSignInAccount account = result.getSignInAccount();
-                if (account != null) {
-                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-                    FirebaseAuth.getInstance()
-                                .signInWithCredential(credential)
-                                .addOnCompleteListener(activity, task -> {
-                                    if (!task.isSuccessful()) {
-                                        LOG.e("Firebase Sign-In failed.", task.getException());
-                                    }
-                                    view.hideProgressDialog();
-                                });
-                } else {
-                    LOG.e("GoogleSignInAccount is null.");
-                    view.showSnackbar(R.string.snackbar_google_sign_in_failed);
-                }
-            } else {
-                LOG.e("Google Sign-In failed.");
-                view.showSnackbar(R.string.snackbar_google_sign_in_failed);
-            }
-        } else if (resultCode == Activity.RESULT_CANCELED) {
-            LOG.i("Google Sign-In canceled.");
+        if (resultCode != Activity.RESULT_OK) {
+            LOG.d("Canceled to sign in to Google.");
             view.showSnackbar(R.string.snackbar_google_sign_in_reqiured);
         }
 
+        GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        if (!googleSignInResult.isSuccess()) {
+            LOG.e("Failed to sign in to Google.");
+            view.showSnackbar(R.string.snackbar_google_sign_in_failed);
+        }
+
+        GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+        if (googleSignInAccount == null) {
+            LOG.e("GoogleSignInAccount is null");
+            view.showSnackbar(R.string.snackbar_google_sign_in_failed);
+        }
+
+        // If the sign-in is successful, Firebase's callback is called first rather than RX's processing
+        // so that the dialog is closed with Firebase's callback.
+        view.showProgressDialog();
+
+        Subscription subscription = signInToFirebaseUseCase
+                .execute(googleSignInAccount)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(authResult -> {
+                    // As the main thread is called first, the fragments are discarded in Firebase's callback,
+                    // so the RX processing is also canceled and will not be called here.
+                }, e -> {
+                    LOG.e("Failed to sign in to Firebase.", e);
+                    view.hideProgressDialog();
+                });
+        compositeSubscription.add(subscription);
     }
 }
