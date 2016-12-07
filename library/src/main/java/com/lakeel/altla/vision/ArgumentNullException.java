4238540956cfef8bb303425491980f660cf8751d
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2016-12-07 20:21:58 +0900

    Squashed 'subtree/vision-core/' changes from e933c31..6ca898b
    6ca898b [ADD] Implement the exception to check null arguments
    
    git-subtree-dir: subtree/vision-core
    git-subtree-split: 6ca898bae760a114c957a825a253f39d6ca706f8

---
+++ library/src/main/java/com/lakeel/altla/vision/ArgumentNullException.java
@@ -0,0 +1,8 @@
+package com.lakeel.altla.vision;
+
+public class ArgumentNullException extends IllegalArgumentException {
+
+    public ArgumentNullException(String paramName) {
+        super(paramName);
+    }
+}
