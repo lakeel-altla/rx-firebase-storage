960c1493ae668a83546712430bb759c040f7ff7d
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2016-12-14 17:34:28 +0900

    [REFACTOR] Remove obsolete subtrees

--- subtree/rx-firebase-storage/library/src/main/java/com/lakeel/altla/rx/firebase/storage/OnProgressListener.java
+++
@@ -1,6 +0,0 @@
-package com.lakeel.altla.rx.firebase.storage;
-
-public interface OnProgressListener {
-
-    void onProgress(long totalBytes, long bytesTransferred);
-}
--- subtree/rx-gms-tasks/README.md
+++
@@ -1,1 +0,0 @@
-# rx-gms-tasks
